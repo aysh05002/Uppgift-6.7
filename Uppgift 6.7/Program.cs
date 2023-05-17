f78144ce44b3e3de16ba54fae4e94bf16227bba5
Admin <Admin@TEB123008>
2023-05-17 10:07:13 +0200

    Add project files.

---
+++ Uppgift 6.7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class progarm
+{
+    static void tal(int prim)
+    {
+        if (prim > 3)
+        {
+            if (prim % 2 == 0 || prim % 3 == 0)
+            {
+                Console.WriteLine($"{prim} är inte ett primt tal");
+            }
+            else
+            {
+                Console.WriteLine($"{prim} är ett primt tal");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"{prim} är ett primt tal");
+        }
+    }
+    static void Main(string[] args)
+    {
+        Console.Write("Undersök om x är ett prim tal \nx = ");
+        tal(Convert.ToInt32(Console.ReadLine()));
+    }
+}
